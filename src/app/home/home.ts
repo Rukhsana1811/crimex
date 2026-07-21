b5b6dbc18141ff8abdbbf3113fbf282f5ac80fc7
Rukhsana Khan <rukhsanakhan1811@gmail.com>
2026-07-22 00:16:52 +0530

    .

---
+++ src/app/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Layout } from '../layout/layout';
+
+@Component({
+  selector: 'app-home',
+  imports: [Layout],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
